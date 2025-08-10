6e7978cb56fca0e6008549904d97f9ba340a5197
olgatenison <88651744+olgatenison@users.noreply.github.com>
2025-08-10 21:51:44 +0300

    page meta

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,34 @@ import Help from "../components/Help";
 import GetInTouch from "../components/GetInTouch";
 import Button from "../components/Button";
 import Cookie from "@/components/Cookie";
+export const metadata = {
+  title: "Остеопатия в Таллине | Руслан Гулишевский",
+  description:
+    "Запишитесь на прием к остеопату Руслану Гулишевскому в Таллине для профилактики заболеваний опорно-двигательного аппарата.",
+  keywords:
+    "остеопатия, здоровье, Таллин, Руслан Гулишевский, остеопат, массаж, реабилитация",
+  author: "Ольга Тенисон",
+  openGraph: {
+    title: "Остеопатия в Таллине | Руслан Гулишевский",
+    description:
+      "Запишитесь на прием к остеопату Руслану Гулишевскому в Таллине для лечения и профилактики заболеваний опорно-двигательного аппарата.",
+    url: "https://www.osteobodyhacking.ee",
+    siteName: "Остеопатия в Таллине",
 
+    images: [
+      {
+        url: `https://www.osteobodyhacking.ee/og-image.jpg`,
+        width: 1200,
+        height: 630,
+        alt: "Остеопатия в Таллине | Руслан Гулишевский",
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 export default function Home() {
   return (
     <>
